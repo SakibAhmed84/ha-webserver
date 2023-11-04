94a2c6bcae3d8f67276d78bcc4778b58752b9267
SakibAhmed84 <89894333+SakibAhmed84@users.noreply.github.com>
2023-11-04 18:00:43 +0000

    Update secrets.tf

--- secrets.tf
+++ secrets.tf
@@ -1,5 +1,6 @@
 # This File is for keeping the Secrets locally as variables away from the other TF Files. 
 # This File must be updated with the appropriate Access Key and Secret Key locally
+# Pulling the Repository will overwrite the local file therefore in that case it will need to be updated again
 
 variable "access_key" {
   description = "AWS Access Key"
